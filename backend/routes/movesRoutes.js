4f7eeb6648a0d4684ed54ccedeca3b5eed0b2d05
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-11 10:47:32 +0100

    created routes file

--- backend/routes/movesRoutes.js
+++ backend/routes/movesRoutes.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/moves", (req, res) => {
-  res.status(200).json({ mssg: "get moves" });
+router.get("/", (req, res) => {
+  res.json({ mssg: "get moves" });
 });
+
+router.post("/", (req, res) => {
+  res.status(200).json({ mssg: "post moves" });
+});
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ mssg: "delete moves" });
+});
+router.put("/:id", (req, res) => {
+  res.status(200).json({ mssg: "update moves" });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 
+const movesRoutes = require("./routes/movesRoutes");
 const port = process.env.PORT;
 const app = express();
+
+app.use("/api/moves", movesRoutes);
 
 app.listen(port, () => console.log(`server staterd on ${port}`));
