d451c351d72a12cd1da1b208473dc183d3f40ee8
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-11 10:14:19 +0100

    set up api test on postman

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("new here");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT;
+const app = express();
+app.get("/api/moves", (req, res) => {
+  res.json({ mssg: "get moves" });
+});
+app.listen(port, () => console.log(`server staterd on ${port}`));
