2fbbff9ce12543357bf4c7d54edfecbae371cb85
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-13 08:46:01 +0100

    validating form

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,19 +2,32 @@
 // instead of having it in the routes folder, we create its own folder
 //and then export it and call it in the routes folder
 
-const jwt = requite("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 const bcrypt = require("bcrypt");
 const { createJWT } = require("../utils/auth");
 
+const emailRegexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
 const signup = (req, res) => {
-  //making sure all feilds have a value
-  if (!name || !email || !password) {
-    res.status(400);
-    console.log("fill all feilds");
+  let { name, email, password, password_confirmation } = req.body;
+
+  // validating feilds
+  let errors = [];
+  if (!name) {
+    errors.push({ name: "required" });
+  }
+  if (!email) {
+    errors.push({ email: "required" });
+  }
+  if (!emailRegexp.test(email)) {
+    errors.push({ email: "invalid" });
+  }
+  if (!password) {
+    errors.push({ password: "required" });
   }
 
-  let { name, email, password, password_confirmation } = req.body;
   User.findOne({ email: email })
     .then((user) => {
       if (user) {
@@ -57,50 +70,48 @@ const signup = (req, res) => {
 
 //login
 const login = (req, res) => {
-  exports.signin = (req, res) => {
-    let { email, password } = req.body;
-    User.findOne({ email: email })
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({
-            errors: [{ user: "not found" }],
-          });
-        } else {
-          bcrypt
-            .compare(password, user.password)
-            .then((isMatch) => {
-              if (!isMatch) {
-                return res
-                  .status(400)
-                  .json({ errors: [{ password: "incorrect" }] });
-              }
-              let access_token = createJWT(user.email, user._id, 3600);
-              jwt.verify(
-                access_token,
-                process.env.TOKEN_SECRET,
-                (err, decoded) => {
-                  if (err) {
-                    res.status(500).json({ erros: err });
-                  }
-                  if (decoded) {
-                    return res.status(200).json({
-                      success: true,
-                      token: access_token,
-                      message: user,
-                    });
-                  }
+  let { email, password } = req.body;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({
+          errors: [{ user: "not found" }],
+        });
+      } else {
+        bcrypt
+          .compare(password, user.password)
+          .then((isMatch) => {
+            if (!isMatch) {
+              return res
+                .status(400)
+                .json({ errors: [{ password: "incorrect" }] });
+            }
+            let access_token = createJWT(user.email, user._id, 3600);
+            jwt.verify(
+              access_token,
+              process.env.TOKEN_SECRET,
+              (err, decoded) => {
+                if (err) {
+                  res.status(500).json({ erros: err });
                 }
-              );
-            })
-            .catch((err) => {
-              res.status(500).json({ erros: err });
-            });
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({ erros: err });
-      });
-  };
+                if (decoded) {
+                  return res.status(200).json({
+                    success: true,
+                    token: access_token,
+                    message: user,
+                  });
+                }
+              }
+            );
+          })
+          .catch((err) => {
+            res.status(500).json({ erros: err });
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ erros: err });
+    });
 };
 
 module.exports = { signup, login };
