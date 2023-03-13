198dbb078c68bff8fd625bfa587e7b8fcf3ed10b
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-13 08:22:06 +0100

    signup and sign in logic

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -8,45 +8,99 @@ const bcrypt = require("bcrypt");
 const { createJWT } = require("../utils/auth");
 
 const signup = (req, res) => {
-  const { email, password, name } = req.body;
-
   //making sure all feilds have a value
   if (!name || !email || !password) {
     res.status(400);
     console.log("fill all feilds");
   }
 
-  //mongoose method schema stuvv
-  const userExists = User.findOne({ email });
-
-  if (userExists) {
-    res.status(400);
-    console.log("email in use");
-  }
-
-  //hashing password
-  const salt = bcrypt.genSalt(10);
-  const hashPassword = bcrypt.hash(password, salt);
-
-  // creating new user
-  const user = new User({
-    name,
-    email,
-    password: hashPassword,
-  });
-
-  if (user) {
-    res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
+  let { name, email, password, password_confirmation } = req.body;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        return res
+          .status(422)
+          .json({ errors: [{ user: "email already exists" }] });
+      } else {
+        const user = new User({
+          name: name,
+          email: email,
+          password: password,
+        });
+        bcrypt.genSalt(10, function (err, salt) {
+          bcrypt.hash(password, salt, function (err, hash) {
+            if (err) throw err;
+            user.password = hash;
+            user
+              .save()
+              .then((response) => {
+                res.status(200).json({
+                  success: true,
+                  result: response,
+                });
+              })
+              .catch((err) => {
+                res.status(500).json({
+                  errors: [{ error: err }],
+                });
+              });
+          });
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errors: [{ error: "Something went wrong" }],
+      });
     });
-  }
 };
 
 //login
 const login = (req, res) => {
-  res.status(200).json({ mssg: "post moves" });
+  exports.signin = (req, res) => {
+    let { email, password } = req.body;
+    User.findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({
+            errors: [{ user: "not found" }],
+          });
+        } else {
+          bcrypt
+            .compare(password, user.password)
+            .then((isMatch) => {
+              if (!isMatch) {
+                return res
+                  .status(400)
+                  .json({ errors: [{ password: "incorrect" }] });
+              }
+              let access_token = createJWT(user.email, user._id, 3600);
+              jwt.verify(
+                access_token,
+                process.env.TOKEN_SECRET,
+                (err, decoded) => {
+                  if (err) {
+                    res.status(500).json({ erros: err });
+                  }
+                  if (decoded) {
+                    return res.status(200).json({
+                      success: true,
+                      token: access_token,
+                      message: user,
+                    });
+                  }
+                }
+              );
+            })
+            .catch((err) => {
+              res.status(500).json({ erros: err });
+            });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ erros: err });
+      });
+  };
 };
 
 module.exports = { signup, login };
