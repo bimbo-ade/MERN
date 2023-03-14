3d17caa52d80db5bd28ad0cb842c5cbad81f2c0d
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-14 11:36:48 +0100

    login styles

--- frontend/frontend/src/components/Login.jsx
+++ frontend/frontend/src/components/Login.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import styled from "styled-components";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <>
+    <Div>
       <img src="/" alt="login" width={900} />
       <div className="overlay"></div>
 
       <div className="cont">
-        <h2>Sign Up</h2>
+        <h2>Login </h2>
 
         <input
           type="text"
@@ -24,12 +25,72 @@ const Login = () => {
           placeholder="password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button>Sign Up</button>
+        <button>Login </button>
         <p>
-          Already have an Account? <span> Login </span>
+          Already have an Account? <span> Signup </span>
         </p>
       </div>
-    </>
+    </Div>
   );
 };
+
+const Div = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  position: relative;
+  color: white;
+
+  .overlay {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+  }
+  .cont {
+    text-align: center;
+    height: auto;
+    width: 390px;
+    background-color: rgba(255, 255, 255, 0.13);
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(4px);
+    box-shadow: 0 0 3px rgba(8, 7, 16, 0.6);
+    padding: 50px 35px;
+    z-index: 3;
+  }
+  input {
+    height: 50px;
+    width: 90%;
+    padding: 0 10px;
+    font-size: 1rem;
+    margin: 1rem auto;
+    outline: none;
+    color: black;
+    border: 1px solid gray;
+    border-radius: 0.1rem;
+  }
+  button {
+    background-color: #ff2156;
+    color: white;
+    border: 0;
+    padding: 0.6rem 1.3rem;
+    margin: 1rem 0;
+  }
+  h2 {
+    // margin-top: 2rem;
+    color: white;
+  }
+  p {
+    color: white;
+    padding-top: 0.3rem;
+    font-size: 0.8rem;
+  }
+`;
 export default Login;
