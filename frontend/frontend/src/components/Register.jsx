0c8957b9c0bbdf127618f6bdde0186109be39199
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-14 11:32:17 +0100

    register styles

--- frontend/frontend/src/components/Register.jsx
+++ frontend/frontend/src/components/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -7,7 +8,7 @@ const Signup = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <>
+    <Div>
       <img src="/" alt="login" width={900} />
       <div className="overlay"></div>
 
@@ -35,7 +36,67 @@ const Signup = () => {
           Already have an Account? <span> Login </span>
         </p>
       </div>
-    </>
+    </Div>
   );
 };
+
+const Div = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  position: relative;
+  color: white;
+
+  .overlay {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+  }
+  .cont {
+    text-align: center;
+    height: auto;
+    width: 390px;
+    background-color: rgba(255, 255, 255, 0.13);
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(4px);
+    box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+    padding: 50px 35px;
+    z-index: 3;
+  }
+  input {
+    height: 50px;
+    width: 90%;
+    padding: 0 10px;
+    font-size: 1rem;
+    margin: 1rem auto;
+    outline: none;
+    color: black;
+    border: 1px solid gray;
+    border-radius: 0.1rem;
+  }
+  button {
+    background-color: #ff2156;
+    color: white;
+    border: 0;
+    padding: 0.6rem 1.3rem;
+    margin: 1rem 0;
+  }
+  h2 {
+    // margin-top: 2rem;
+    color: white;
+  }
+  p {
+    color: white;
+    padding-top: 0.3rem;
+    font-size: 0.8rem;
+  }
+`;
 export default Signup;
