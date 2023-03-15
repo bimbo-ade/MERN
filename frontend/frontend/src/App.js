f0a44a83af49e63dc0e062fc8ada53cdb1e58d6c
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-15 16:12:59 +0100

    routes

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,13 +1,19 @@
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Signup from "./components/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Signup />
-      <Login />
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
