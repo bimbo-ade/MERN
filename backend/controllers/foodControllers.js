ce0eee48a5b92e0cc54b9e9d56afb58b7937dd12
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-19 18:42:58 +0100

    food post controllers

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -4,7 +4,27 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 
 const getFood = async (req, res) => {};
-const postFood = async (req, res) => {};
+
+const postFood = async (req, res) => {
+  try {
+    let { title, desc, price } = req.body;
+    const food = await Food.create({
+      title,
+      desc,
+      price,
+    });
+    if (food) {
+      res.status(201).json({
+        _id: food.id,
+        title: food.title,
+        desc: food.desc,
+        price: food.price,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "lol for the catch" });
+  }
+};
 const deleteFood = async (req, res) => {};
 const updateFood = async (req, res) => {};
 
