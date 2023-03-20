9a007d8f59f8f1cdfa718c068d8ef50ccb6d945e
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-20 09:00:54 +0100

    food controllers updated

--- backend/routes/FoodRoutes.js
+++ backend/routes/FoodRoutes.js
@@ -7,8 +7,12 @@ const {
   updateFood,
 } = require("../controllers/foodControllers");
 
+router.get("/", getFood);
+router.post("/", postFood);
+router.post("/:id", deleteFood);
+router.post("/:id", updateFood);
 //A cleaner way to write the above code
-router.route("/").get("/", getFood).post("/", postFood);
-router.route("/:id").delete("/:id", deleteFood).put("/:id", updateFood);
+// router.route("/").get("/", getFood).post("/", postFood);
+// router.route("/:id").delete("/:id", deleteFood).put("/:id", updateFood);
 
 module.exports = router;
